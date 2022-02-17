d13b5938e8e55fed6ffe9969a6e791c6e5223595
Alberto Moura <betinhoolxd@gmail.com>
2022-02-17 12:57:26 -0300

    Criando endpoints e métodos CRUD

---
+++ src/routes/users.route.ts
@@ -0,0 +1,41 @@
+import StatusCode from 'http-status-codes'
+import { Router, Request, Response, NextFunction } from 'express'
+
+const usersRoute = Router()
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+	console.log('oiii')
+	const users = [{ id: 1, name: 'Fulano' }]
+	res.status(StatusCode.OK).send(users)
+})
+
+usersRoute.get(
+	'/users/:uuid',
+	(req: Request, res: Response<{ uuid: string }>, next: NextFunction) => {
+		const uuid = req.params.uuid
+		res.status(200).send({ uuid })
+	}
+)
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+	const newUser = req.body
+	res.status(StatusCode.CREATED).send(newUser)
+})
+
+usersRoute.put(
+	'/users/:uuid',
+	(req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+		const uuid = req.params.uuid
+		res.status(StatusCode.OK).send(uuid)
+	}
+)
+
+usersRoute.delete(
+	'/users/:uuid',
+	(req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+		const uuid = req.params.uuid
+		res.status(StatusCode.OK).send(uuid)
+	}
+)
+
+export default usersRoute
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express'
+import usersRoute from './routes/users.route'
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(usersRoute)
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
 	console.log('Request to /status')
