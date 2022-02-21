ec42af76f2b02aff6046f54632bf00eeba2209e7
Alberto Moura <betinhoolxd@gmail.com>
2022-02-21 20:36:19 -0300

    Preparando para deploy

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,6 @@ app.use(bearerAuthMiddleware)
 app.use(usersRoute)
 app.use(errorHandler)
 // Inicilização do servidor
-app.listen(3000, () => {
-	console.log('listening on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`Listening on port ${process.env.PORT || 3000}`)
 })
